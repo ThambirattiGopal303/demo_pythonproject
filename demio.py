f95caff9ebd34b9f6122aa5137221e7911128107
ThambirattiGopal303 <thambirattig2002@gmail.com>
2025-06-01 22:03:50 +0530

    Add files via upload

---
+++ demio.py
@@ -0,0 +1,23 @@
+# print("------ For Loop -------")
+# for i in range(10):
+#     print(i)
+#
+# table=int(input("Enter The Number : "))
+# for i in range(17):
+#     print(i," * ",table," = ",i*table)
+
+
+# for i in range(3):
+#     for j in range(3):
+#         print(" * ",end="")
+#     print()
+
+for i in range(5):
+    name=input("Enter The Name : ")
+    n=int(input("Enter The Age : "))
+    if n>=18:
+        print(name,"Your Eligible To Vote")
+    else:
+        print(name,"Your Are Not Eligible To Vote..")
+
+
